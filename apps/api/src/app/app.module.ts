ad28b8324d676972daa5265b4cf575051656d352
Guilherme Fontebasso <guilhermenishifonte@gmail.com>
2021-09-24 08:36:08 -0300

    chore(api): add typeorm

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { environment as env } from '../environments/environment';
+
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: env.RDS_HOSTNAME,
+      port: +env.RDS_PORT,
+      username: env.RDS_USERNAME,
+      password: env.RDS_PASSWORD,
+      database: env.RDS_DB_NAME,
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/api/src/environments/environment.ts
+++ apps/api/src/environments/environment.ts
@@ -1,3 +1,8 @@
 export const environment = {
   production: false,
+  RDS_HOSTNAME: 'localhost',
+  RDS_PORT: 3306,
+  RDS_USERNAME: 'tester',
+  RDS_PASSWORD: 'test',
+  RDS_DB_NAME: 'test',
 };

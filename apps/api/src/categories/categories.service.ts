bc01c0033cee55b2709b08edcf51eaba2d474cae
Guilherme Fontebasso <guilhermenishifonte@gmail.com>
2021-09-24 17:35:01 -0300

    feat(api): ensure CategoriesServices findAll returns the categories correctly on success

---
+++ apps/api/src/categories/categories.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Category } from './category.entity';
+
+@Injectable()
+export class CategoriesService {
+  constructor(
+    @InjectRepository(Category)
+    private categoriesRepository: Repository<Category>
+  ) {}
+
+  findAll(): Promise<Category[]> {
+    return this.categoriesRepository.find();
+  }
+}

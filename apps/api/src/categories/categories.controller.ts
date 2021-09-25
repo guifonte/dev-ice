a8d43ffd98400b97d42ef5b5b7f307dc579bde15
Guilherme Fontebasso <guilhermenishifonte@gmail.com>
2021-09-25 16:21:27 -0300

    feat(api): prepare categories to testing and develop

--- apps/api/src/categories/categories.controller.ts
+++ apps/api/src/categories/categories.controller.ts
@@ -1,4 +1,7 @@
 import { Controller } from '@nestjs/common';
+import { CategoriesService } from './categories.service';
 
 @Controller('categories')
-export class CategoriesController {}
+export class CategoriesController {
+  constructor(private categoriesService: CategoriesService) {}
+}
--- apps/api/tests/categories.controller.spec.ts
+++ apps/api/tests/categories.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoriesController } from '../src/categories/categories.controller';
+import { CategoriesService } from '../src/categories/categories.service';
 
 describe('CategoriesController', () => {
   let controller: CategoriesController;
@@ -7,6 +8,12 @@ describe('CategoriesController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CategoriesController],
+      providers: [
+        {
+          provide: CategoriesService,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     controller = module.get<CategoriesController>(CategoriesController);

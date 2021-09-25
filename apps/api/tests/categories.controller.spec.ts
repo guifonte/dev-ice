2f0bec8968661d00fb1329146081733b4b0cd417
Guilherme Fontebasso <guilhermenishifonte@gmail.com>
2021-09-25 18:07:20 -0300

    test(api): ensure CategoriesController delete resolves correctly

--- apps/api/tests/categories.controller.spec.ts
+++ apps/api/tests/categories.controller.spec.ts
@@ -1,10 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import faker from 'faker';
+
 import { CategoriesController } from '../src/categories/categories.controller';
 import { CategoriesService } from '../src/categories/categories.service';
-import { getRepositoryToken } from '@nestjs/typeorm';
-import { mockCategory, repositoryMockFactory } from './helpers';
+import { mockCategory, mockId, repositoryMockFactory } from './helpers';
 import { Category } from '../src/categories/category.entity';
 import { Device } from '../src/devices/device.entity';
+import { HttpException, HttpStatus } from '@nestjs/common';
 
 describe('CategoriesController', () => {
   let controller: CategoriesController;
@@ -48,6 +51,48 @@ describe('CategoriesController', () => {
       jest.spyOn(service, 'findAll').mockImplementation(() => result);
 
       expect(await controller.findAll()).toEqual([mockedCat]);
+    });
+  });
+
+  describe('delete', () => {
+    it('should return void if service returns void', async () => {
+      const result = Promise.resolve<void>(undefined);
+      jest.spyOn(service, 'delete').mockImplementation(() => result);
+
+      const mockedId = mockId();
+      expect(await controller.deleteOne(mockedId)).toEqual(undefined);
+      expect(service.delete).toHaveBeenCalledWith(mockedId);
+    });
+
+    it('should throw NO_CONTENT if id does not exists', async () => {
+      jest.spyOn(service, 'delete').mockImplementation(() => {
+        throw new HttpException('', HttpStatus.NO_CONTENT);
+      });
+      const mockedId = mockId();
+
+      expect.assertions(3);
+      try {
+        await controller.deleteOne(mockedId);
+      } catch (error) {
+        expect(error).toBeInstanceOf(HttpException);
+        expect(error).toHaveProperty('status', HttpStatus.NO_CONTENT);
+        expect(service.delete).toHaveBeenCalledWith(mockedId);
+      }
+    });
+    it('should throw CONFLICT if service do so', async () => {
+      jest.spyOn(service, 'delete').mockImplementation(() => {
+        throw new HttpException('', HttpStatus.CONFLICT);
+      });
+      const mockedId = mockId();
+
+      expect.assertions(3);
+      try {
+        await controller.deleteOne(mockedId);
+      } catch (error) {
+        expect(error).toBeInstanceOf(HttpException);
+        expect(error).toHaveProperty('status', HttpStatus.CONFLICT);
+        expect(service.delete).toHaveBeenCalledWith(mockedId);
+      }
     });
   });
 });

f80af445a1f2db0b4e7740016d1ea9bc2d123da4
Guilherme Fontebasso <guilhermenishifonte@gmail.com>
2021-09-25 13:12:35 -0300

    ci(all): add semantic-release with github actions

---
+++ release.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  branches: [
+    '+([0-9])?(.{+([0-9]),x}).x',
+    'master',
+    'next',
+    'next-major',
+    { name: 'beta', prerelease: true },
+    { name: 'alpha', prerelease: true },
+  ],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    '@semantic-release/github',
+    // ["@semantic-release/github", {
+    //   "assets": [
+    //     {path: 'dist/app/api', label: 'DevIce api (${nextRelease.gitTag}) distribution'},
+    //     {path: 'dist/app/web', label: 'DevIce Web app (${nextRelease.gitTag}) distribution'},
+    //   ]
+    // }],
+    '@semantic-release/git',
+  ],
+  preset: 'angular',
+  dryRun: true,
+};

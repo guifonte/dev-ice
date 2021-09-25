419682f57f720e63a323c8477862558325844bdf
Guilherme Fontebasso <guilhermenishifonte@gmail.com>
2021-09-25 14:24:56 -0300

    feat(api): generate CategoriesController and CategoriesModule

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoriesModule } from '../categories/categories.module';
 
 import { environment as env } from '../environments/environment';
 
 @Module({
   imports: [
+    CategoriesModule,
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: env.RDS_HOSTNAME,

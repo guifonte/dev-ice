e98148f18e6c9b272a4768f2fc4b50335e93c835
Guilherme Fontebasso <guilhermenishifonte@gmail.com>
2021-09-25 09:57:03 -0300

    feat(api): ensure DevicesService throw BAD_REQUEST by create when category does not exists

--- apps/api/src/devices/devices.service.ts
+++ apps/api/src/devices/devices.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { Category } from '../categories/category.entity';
 import { CreateDeviceDTO } from './create-device.dto';
 import { Device } from './device.entity';
 
@@ -8,16 +9,24 @@ import { Device } from './device.entity';
 export class DevicesService {
   constructor(
     @InjectRepository(Device)
-    private devicesRepository: Repository<Device>
+    private devicesRepository: Repository<Device>,
+    @InjectRepository(Category)
+    private categoryRepository: Repository<Category>
   ) {}
 
   findAll(): Promise<Device[]> {
     return this.devicesRepository.find();
   }
 
-  create(createDeviceDTO: CreateDeviceDTO): Promise<Device> {
+  async create(createDeviceDTO: CreateDeviceDTO): Promise<Device> {
+    const foundCategory = await this.categoryRepository.findOne(
+      createDeviceDTO.categoryId
+    );
+    if (!foundCategory) {
+      throw new HttpException('Category does no exist', HttpStatus.BAD_REQUEST);
+    }
+
     const partialDevice = this.devicesRepository.create(createDeviceDTO);
-    console.log(partialDevice);
     return this.devicesRepository.save(partialDevice);
   }
 }

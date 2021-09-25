5e1b63a3b4fdaca359d2a073624bf460e0cd3129
Guilherme Fontebasso <guilhermenishifonte@gmail.com>
2021-09-25 17:18:31 -0300

    feat(api): ensure CategoriesController findAll returns correctly values

--- apps/api/src/categories/categories.controller.ts
+++ apps/api/src/categories/categories.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
+import { Category } from './category.entity';
 
 @Controller('categories')
 export class CategoriesController {
   constructor(private categoriesService: CategoriesService) {}
+
+  @Get()
+  findAll(): Promise<Category[]> {
+    return this.categoriesService.findAll();
+  }
 }

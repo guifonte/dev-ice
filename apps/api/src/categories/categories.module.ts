95f1eb5a263980f9eb8aefa27007a5a34f705838
Guilherme Fontebasso <guilhermenishifonte@gmail.com>
2021-09-25 22:38:42 -0300

    feat(api): add TypeOrmModeule configuration in CategoriesModule

--- apps/api/src/categories/categories.module.ts
+++ apps/api/src/categories/categories.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Device } from '../devices/device.entity';
 import { CategoriesController } from './categories.controller';
 import { CategoriesService } from './categories.service';
+import { Category } from './category.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Category, Device])],
   controllers: [CategoriesController],
   providers: [CategoriesService],
 })

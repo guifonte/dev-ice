d6d1501db92743380a289b8f61a70765c74037ad
Guilherme Fontebasso <guilhermenishifonte@gmail.com>
2021-09-25 23:27:50 -0300

    test(api): ensure CategoriesController returns 400 if DTO name field is not string

--- apps/api/tests/categories.e2e-spec.ts
+++ apps/api/tests/categories.e2e-spec.ts
@@ -37,25 +37,74 @@ describe('CategoriesModule (e2e)', () => {
     await app.init();
   });
 
-  it('/categories (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/categories')
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .expect([]);
+  describe('GET /categories', () => {
+    it('should receive empty array', (done) => {
+      request(app.getHttpServer())
+        .get('/categories')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .expect([], done);
+    });
+
+    it('should receive array with only the created Category', (done) => {
+      const mockedCreateCatDTO: CreateCategoryDTO = {
+        name: faker.random.word(),
+      };
+      let receivedCat: Category;
+      request(app.getHttpServer())
+        .post('/categories')
+        .send(mockedCreateCatDTO)
+        .expect(201)
+        .then((res) => {
+          receivedCat = res.body;
+
+          request(app.getHttpServer())
+            .get('/categories')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .expect((res) => {
+              expect(res.body).toEqual([receivedCat]);
+            })
+            .end((err) => {
+              if (err) return done(err);
+              return done();
+            });
+        });
+    });
   });
 
-  it('/categories (POST)', () => {
-    const mockedCreateCatDTO: CreateCategoryDTO = { name: faker.random.word() };
-    return request(app.getHttpServer())
-      .post('/categories')
-      .send(mockedCreateCatDTO)
-      .expect('Content-Type', /json/)
-      .expect(201)
-      .expect((res) => {
-        expect(res.body.id).toBe(1);
-        expect(res.body.name).toEqual(mockedCreateCatDTO.name);
-      });
+  describe('POST /categories', () => {
+    it('should return a Category with same name as the DTO and id 1', (done) => {
+      const mockedCreateCatDTO: CreateCategoryDTO = {
+        name: faker.random.word(),
+      };
+      request(app.getHttpServer())
+        .post('/categories')
+        .send(mockedCreateCatDTO)
+        .expect('Content-Type', /json/)
+        .expect(201)
+        .expect((res) => {
+          expect(res.body.id).toBe(1);
+          expect(res.body.name).toEqual(mockedCreateCatDTO.name);
+        })
+        .end((err) => {
+          if (err) return done(err);
+          return done();
+        });
+    });
+
+    it('should return BAD_REQUEST (400) if name is not string ', (done) => {
+      const wrongCreateCatDTO = { name: 1 };
+      request(app.getHttpServer())
+        .post('/categories')
+        .send(wrongCreateCatDTO)
+        .expect('Content-Type', /json/)
+        .expect(400)
+        .end((err) => {
+          if (err) return done(err);
+          return done();
+        });
+    });
   });
 
   afterEach(async () => {

7c28220e7dba1b048686a4cf5668aebf54b56c32
Guilherme Fontebasso <guilhermenishifonte@gmail.com>
2021-09-26 10:54:53 -0300

    feat(api): ensure DevicesController findAll returns properly

--- apps/api/src/devices/devices.controller.ts
+++ apps/api/src/devices/devices.controller.ts
@@ -1,8 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { Device } from './device.entity';
 
 import { DevicesService } from './devices.service';
 
 @Controller('devices')
 export class DevicesController {
   constructor(private devicesService: DevicesService) {}
+
+  @Get()
+  findAll(): Promise<Device[]> {
+    return this.devicesService.findAll();
+  }
 }

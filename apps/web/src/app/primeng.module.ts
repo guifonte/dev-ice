d45670673a477d065d3be54868400e2ff5213626
Guilherme Fontebasso <guilhermenishifonte@gmail.com>
2021-09-27 20:47:36 -0300

    feat(web): add responsive github button to the header

--- apps/web/src/app/primeng.module.ts
+++ apps/web/src/app/primeng.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { MenubarModule } from 'primeng/menubar';
 import { TabMenuModule } from 'primeng/tabmenu';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
-  exports: [MenubarModule, TabMenuModule],
+  exports: [MenubarModule, TabMenuModule, ButtonModule],
 })
 export class PrimengModule {}

681a751e30057a24c3a697d473e1c5e37f32f462
Guilherme Fontebasso <guilhermenishifonte@gmail.com>
2021-09-27 19:29:14 -0300

    feat(web): make category-list subscribe to category service and show the list of category

--- apps/web/src/app/category-list/category-list.component.ts
+++ apps/web/src/app/category-list/category-list.component.ts
@@ -1,14 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { Category } from '@dev-ice/domain';
+import { CategoryService } from '../category.service';
 
 @Component({
   selector: 'dev-ice-category-list',
   templateUrl: './category-list.component.html',
   styleUrls: ['./category-list.component.scss'],
 })
-export class CategoryListComponent implements OnInit {
-  // constructor() { }
+export class CategoryListComponent implements OnInit, OnDestroy {
+  categories: Category[] = [];
+
+  private categorySub!: Subscription;
+  constructor(public categoryService: CategoryService) {}
 
   ngOnInit(): void {
-    console.log('init');
+    this.categorySub = this.categoryService
+      .getCategoriesUpdateListener()
+      .subscribe((categories) => {
+        this.categories = categories;
+        console.log(this.categories);
+      });
+    this.categoryService.getCategories();
+  }
+
+  ngOnDestroy(): void {
+    this.categorySub.unsubscribe();
   }
 }

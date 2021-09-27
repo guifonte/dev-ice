3f970f52e6bf382259ff103a875336b0cac6d6cd
Guilherme Fontebasso <guilhermenishifonte@gmail.com>
2021-09-27 19:26:34 -0300

    feat(web): make device-list subscribe to device service and show the list of devices

--- apps/web/src/app/device-list/device-list.component.ts
+++ apps/web/src/app/device-list/device-list.component.ts
@@ -1,14 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { Device } from '@dev-ice/domain';
+import { DeviceService } from '../device.service';
 
 @Component({
   selector: 'dev-ice-device-list',
   templateUrl: './device-list.component.html',
   styleUrls: ['./device-list.component.scss'],
 })
-export class DeviceListComponent implements OnInit {
-  // constructor() { }
+export class DeviceListComponent implements OnInit, OnDestroy {
+  devices: Device[] = [];
+
+  private deviceSub!: Subscription;
+  constructor(public deviceService: DeviceService) {}
 
   ngOnInit(): void {
-    console.log('init');
+    this.deviceSub = this.deviceService
+      .getDevicesUpdateListener()
+      .subscribe((devices) => {
+        this.devices = devices;
+        console.log(this.devices);
+      });
+    this.deviceService.getDevices();
+  }
+
+  ngOnDestroy(): void {
+    this.deviceSub.unsubscribe();
   }
 }

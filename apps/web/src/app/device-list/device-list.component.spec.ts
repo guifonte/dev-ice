55ee2f05f3cb3927de8a2bb0d2fff8f305c49c9a
Guilherme Fontebasso <guilhermenishifonte@gmail.com>
2021-09-27 19:27:29 -0300

    test(web): make device-list subscribe to device service and show the list of devices

--- apps/web/src/app/device-list/device-list.component.spec.ts
+++ apps/web/src/app/device-list/device-list.component.spec.ts
@@ -1,4 +1,6 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DeviceService } from '../device.service';
 
 import { DeviceListComponent } from './device-list.component';
 
@@ -8,7 +10,9 @@ describe('DeviceListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientModule],
       declarations: [DeviceListComponent],
+      providers: [DeviceService],
     }).compileComponents();
   });
 

ed80255a299f70b4ce52f9c9f44cb7241089e31f
Guilherme Fontebasso <guilhermenishifonte@gmail.com>
2021-09-27 00:17:30 -0300

    feat(domain): add CreateCategoryDTO

--- libs/domain/src/index.ts
+++ libs/domain/src/index.ts
@@ -1,2 +1,3 @@
 export * from './lib/api-interfaces';
 export * from './lib/categories.model';
+export * from './lib/create-category.dto';
---
+++ libs/domain/src/lib/create-category.dto.ts
@@ -0,0 +1,3 @@
+export interface CreateCategoryDTO {
+  name: string;
+}

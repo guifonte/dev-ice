20c2d2b2e257cee6067369afc35579da16217f5e
Guilherme Fontebasso <guilhermenishifonte@gmail.com>
2021-09-27 19:31:16 -0300

    feat(web): make header consume router provider

--- apps/web/src/app/header/header.component.ts
+++ apps/web/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'dev-ice-header',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  // constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
     console.log('init');

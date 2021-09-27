8d3415b12c22ae72a32fc3c4f556841eb4d50262
Guilherme Fontebasso <guilhermenishifonte@gmail.com>
2021-09-27 15:54:36 -0300

    test(api): remove faker from color creation, because it can have white space. Use green instead

--- apps/api/tests/helpers/mock-device.ts
+++ apps/api/tests/helpers/mock-device.ts
@@ -1,11 +1,10 @@
-import faker from 'faker';
 import { mockCategory, mockId } from '.';
 import { Device } from '../../src/devices/device.entity';
 
 export const mockDevice = (): Device => {
   const device = new Device();
   device.id = mockId();
-  device.color = faker.commerce.color();
+  device.color = 'green';
   device.partNumber = mockId();
   device.category = mockCategory();
   return device;

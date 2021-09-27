528c2c0c6317db529e771346fc5fa06ddf58122e
Guilherme Fontebasso <guilhermenishifonte@gmail.com>
2021-09-27 19:57:41 -0300

    feat(web): implement the header, with responsiveness

--- apps/web/src/app/header/header.component.ts
+++ apps/web/src/app/header/header.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { MenuItem } from 'primeng/api';
+
 @Component({
   selector: 'dev-ice-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  items!: MenuItem[];
+
   constructor(private router: Router) {}
 
-  ngOnInit(): void {
-    console.log('init');
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Devices',
+        routerLink: '/devices',
+        routerLinkActiveOptions: { exact: true },
+        expanded: this.checkActiveState('/devices'),
+      },
+      {
+        label: 'Categories',
+        routerLink: '/categories',
+        routerLinkActiveOptions: { exact: true },
+        expanded: this.checkActiveState('/categories'),
+      },
+    ];
+  }
+
+  checkActiveState(givenLink: string) {
+    console.log(this.router.url);
+    if (this.router.url.indexOf(givenLink) === -1) {
+      return false;
+    } else {
+      return true;
+    }
   }
 }

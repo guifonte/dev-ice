367fb96317523703c46724864d785e37ed0933ab
Guilherme Fontebasso <guilhermenishifonte@gmail.com>
2021-09-27 16:36:31 -0300

    test(web): ensure DeviceService handles createDevice correctly

--- apps/web/src/app/device.service.spec.ts
+++ apps/web/src/app/device.service.spec.ts
@@ -11,7 +11,11 @@ import { DeviceService } from './device.service';
 import { ErrorInterceptor } from './error-interceptor';
 
 import { environment as env } from '../environments/environment';
-import { mockDevice } from '@dev-ice/testing';
+import {
+  fromMockedDeviceToMockedCreateDeviceDTO,
+  mockCreateDeviceDTO,
+  mockDevice,
+} from '@dev-ice/testing';
 
 const url = env.baseURL + '/api/devices';
 
@@ -107,6 +111,82 @@ describe('DeviceService', () => {
 
       const req = httpTestingController.expectOne(url);
       expect(req.request.method).toEqual('GET');
+      const emsg = 'deliberate 404 error';
+      req.flush(emsg, { status: 404, statusText: 'Not Found' });
+    });
+  });
+
+  describe('createDevice', () => {
+    it('should return a list with one device if devices list is empty', (done) => {
+      const mockedDevice: Device = mockDevice();
+      const mockedDTO = fromMockedDeviceToMockedCreateDeviceDTO(mockedDevice);
+
+      deviceSubs = devicesUpdateListener.subscribe({
+        next: (value) => {
+          try {
+            expect(value).toEqual([mockedDevice]);
+            done();
+          } catch (error) {
+            done(error);
+          }
+        },
+        error: (err) => done(err),
+      });
+
+      service.createDevice(mockedDTO);
+
+      const req = httpTestingController.expectOne(url);
+      expect(req.request.method).toEqual('POST');
+      req.flush(mockedDevice);
+    });
+
+    it('should return a list with two device if device list had only a device', (done) => {
+      const mockedDevice: Device = mockDevice();
+      const mockedDevice2: Device = mockDevice();
+      const mockedDTO = fromMockedDeviceToMockedCreateDeviceDTO(mockedDevice);
+      const mockedDTO2 = fromMockedDeviceToMockedCreateDeviceDTO(mockedDevice2);
+
+      service.createDevice(mockedDTO);
+      service.createDevice(mockedDTO2);
+
+      const req = httpTestingController.match(url);
+      expect(req.length).toBe(2);
+      expect(req[0].request.method).toEqual('POST');
+      req[0].flush(mockedDevice);
+
+      deviceSubs = devicesUpdateListener.subscribe({
+        next: (value) => {
+          try {
+            expect(value).toEqual([mockedDevice, mockedDevice2]);
+            done();
+          } catch (error) {
+            done(error);
+          }
+        },
+        error: (err) => done(err),
+      });
+
+      req[1].flush(mockedDevice2);
+    });
+
+    it('should return error to subscription', (done) => {
+      const mockedDTO = mockCreateDeviceDTO();
+
+      deviceSubs = devicesUpdateListener.subscribe({
+        next: () => {
+          done('should not be here');
+        },
+        error: (err: HttpErrorResponse) => {
+          expect(err.status).toEqual(404);
+          expect(err.error).toEqual(emsg);
+          done();
+        },
+      });
+
+      service.createDevice(mockedDTO);
+
+      const req = httpTestingController.expectOne(url);
+      expect(req.request.method).toEqual('POST');
       const emsg = 'deliberate 404 error';
       req.flush(emsg, { status: 404, statusText: 'Not Found' });
     });

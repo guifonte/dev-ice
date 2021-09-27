274f48765f53e112caf535bd0a8eb1c9de564266
Guilherme Fontebasso <guilhermenishifonte@gmail.com>
2021-09-27 15:58:40 -0300

    test(web): ensure DeviceService handles correctly getDevices

--- apps/web/src/app/device.service.spec.ts
+++ apps/web/src/app/device.service.spec.ts
@@ -11,8 +11,9 @@ import { DeviceService } from './device.service';
 import { ErrorInterceptor } from './error-interceptor';
 
 import { environment as env } from '../environments/environment';
+import { mockDevice } from '@dev-ice/testing';
 
-const url = env.baseURL + '/api/categories';
+const url = env.baseURL + '/api/devices';
 
 describe('DeviceService', () => {
   let service: DeviceService;
@@ -45,5 +46,69 @@ describe('DeviceService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('getDevices', () => {
+    it('should return [] if server respond with []', (done) => {
+      const expectedDevices: Device[] = [];
+      deviceSubs = devicesUpdateListener.subscribe({
+        next: (value) => {
+          try {
+            expect(value).toEqual(expectedDevices);
+            done();
+          } catch (error) {
+            done(error);
+          }
+        },
+        error: (err) => done(err),
+      });
+
+      service.getDevices();
+
+      const req = httpTestingController.expectOne(url);
+      expect(req.request.method).toEqual('GET');
+      req.flush(expectedDevices);
+    });
+
+    it('should return a list with one device if server respond with a list with one device', (done) => {
+      const expectedDevices: Device[] = [mockDevice()];
+      deviceSubs = devicesUpdateListener.subscribe({
+        next: (value) => {
+          try {
+            expect(value).toEqual(expectedDevices);
+            done();
+          } catch (error) {
+            done(error);
+          }
+        },
+        error: (err) => done(err),
+      });
+
+      service.getDevices();
+
+      const req = httpTestingController.expectOne(url);
+      expect(req.request.method).toEqual('GET');
+      req.flush(expectedDevices);
+    });
+
+    it('should return error to subscription', (done) => {
+      deviceSubs = devicesUpdateListener.subscribe({
+        next: () => {
+          done('should not be here');
+        },
+        error: (err: HttpErrorResponse) => {
+          expect(err.status).toEqual(404);
+          expect(err.error).toEqual(emsg);
+          done();
+        },
+      });
+
+      service.getDevices();
+
+      const req = httpTestingController.expectOne(url);
+      expect(req.request.method).toEqual('GET');
+      const emsg = 'deliberate 404 error';
+      req.flush(emsg, { status: 404, statusText: 'Not Found' });
+    });
   });
 });

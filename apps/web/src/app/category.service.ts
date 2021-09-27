0e94a8dcbacc99f41c34ed885f60673147670f02
Guilherme Fontebasso <guilhermenishifonte@gmail.com>
2021-09-27 02:52:46 -0300

    feat(web): add Observer pattern to getCategories

--- apps/web/src/app/category.service.ts
+++ apps/web/src/app/category.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Category, CreateCategoryDTO } from '@dev-ice/domain';
+import { Subject } from 'rxjs';
 
 const url = '/api/categories';
 
@@ -8,10 +9,20 @@ const url = '/api/categories';
   providedIn: 'root',
 })
 export class CategoryService {
+  private categories: Category[] = [];
+  private categoriesUpdated = new Subject<Category[]>();
+
   constructor(private http: HttpClient) {}
 
+  getCategoriesUpdateListener() {
+    return this.categoriesUpdated.asObservable();
+  }
+
   getCategories() {
-    return this.http.get<Category[]>(url);
+    this.http.get<Category[]>(url).subscribe((res) => {
+      this.categories = res;
+      this.categoriesUpdated.next([...this.categories]);
+    });
   }
 
   createCategory(createCategoryDTO: CreateCategoryDTO) {

db8f7e39e30e8132da8749fbd393d37bc4ac4104
Guilherme Fontebasso <guilhermenishifonte@gmail.com>
2021-09-27 19:33:12 -0300

    test(web): fix declarations and imports

--- apps/web/src/app/app.component.spec.ts
+++ apps/web/src/app/app.component.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 describe('AppComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [AppComponent],
-      imports: [],
+      declarations: [AppComponent, HeaderComponent],
+      imports: [RouterTestingModule],
     }).compileComponents();
   });
 

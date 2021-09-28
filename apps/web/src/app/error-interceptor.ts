9b94aac11110d4cac762a261981d2ba6a28b4bae
Guilherme Fontebasso <guilhermenishifonte@gmail.com>
2021-09-27 23:14:03 -0300

    feat(web): add toast and message service

--- apps/web/src/app/error-interceptor.ts
+++ apps/web/src/app/error-interceptor.ts
@@ -6,14 +6,18 @@ import {
 } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
+import { MessageService } from 'primeng/api';
+import { Injectable } from '@angular/core';
 
+@Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
+  constructor(private messageService: MessageService) {}
   // prettier-ignore
   intercept(req: HttpRequest<any>, next: HttpHandler) { // eslint-disable-line
     return next.handle(req).pipe(
       catchError((error: HttpErrorResponse) => {
         console.log(error);
-        // alert(error.error.message);
+        this.messageService.add({severity:'error', summary: 'Something wrong happened', detail: error.message, life: 3000})
         return throwError(() => error);
       })
     );
--- apps/web/src/app/primeng.module.ts
+++ apps/web/src/app/primeng.module.ts
@@ -2,8 +2,20 @@ import { NgModule } from '@angular/core';
 import { MenubarModule } from 'primeng/menubar';
 import { TabMenuModule } from 'primeng/tabmenu';
 import { ButtonModule } from 'primeng/button';
+import { ToastModule } from 'primeng/toast';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { TableModule } from 'primeng/table';
+import { ConfirmationService, MessageService } from 'primeng/api';
 
 @NgModule({
-  exports: [MenubarModule, TabMenuModule, ButtonModule],
+  exports: [
+    MenubarModule,
+    TabMenuModule,
+    ButtonModule,
+    ToastModule,
+    ConfirmDialogModule,
+    TableModule,
+  ],
+  providers: [MessageService, ConfirmationService],
 })
 export class PrimengModule {}

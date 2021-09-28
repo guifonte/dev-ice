8949a9fe2502524a8ae6712ef0a3fb8945f83999
Guilherme Fontebasso <guilhermenishifonte@gmail.com>
2021-09-28 02:07:47 -0300

    test(web): add message and confirmation services

--- apps/web/src/app/category-list/category-list.component.spec.ts
+++ apps/web/src/app/category-list/category-list.component.spec.ts
@@ -1,5 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ConfirmationService, MessageService } from 'primeng/api';
 import { CategoryService } from '../category.service';
 
 import { CategoryListComponent } from './category-list.component';
@@ -12,7 +13,7 @@ describe('CategoryListComponent', () => {
     await TestBed.configureTestingModule({
       imports: [HttpClientModule],
       declarations: [CategoryListComponent],
-      providers: [CategoryService],
+      providers: [CategoryService, ConfirmationService, MessageService],
     }).compileComponents();
   });
 
--- apps/web/src/app/device-list/device-list.component.spec.ts
+++ apps/web/src/app/device-list/device-list.component.spec.ts
@@ -1,5 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ConfirmationService, MessageService } from 'primeng/api';
 import { DeviceService } from '../device.service';
 
 import { DeviceListComponent } from './device-list.component';
@@ -12,7 +13,7 @@ describe('DeviceListComponent', () => {
     await TestBed.configureTestingModule({
       imports: [HttpClientModule],
       declarations: [DeviceListComponent],
-      providers: [DeviceService],
+      providers: [DeviceService, ConfirmationService, MessageService],
     }).compileComponents();
   });
 
